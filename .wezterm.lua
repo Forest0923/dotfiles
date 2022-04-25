48b599252ecafcb38f982951ecaa12dbd228d4ef
Forest0923 <tropicalrainforest23@gmail.com>
2022-04-25 21:29:17 +0900

    Update

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,12 +4,5 @@ return {
 	text_background_opacity		= 1.0,
 	window_decorations		= "NONE",
 	enable_tab_bar			= false,
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 	exit_behavior			= "Close",
->>>>>>> f8e852c (Add a configuration file for wezterm, terminal emulator)
-=======
-	exit_behavior			= "Close",
->>>>>>> 51dd75d (Add a configuration file for wezterm, terminal emulator)
 }
