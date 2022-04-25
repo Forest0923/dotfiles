647ef3a6fd2049afa6d775334619eb5fa87b993b
Forest0923 <tropicalrainforest23@gmail.com>
2022-04-25 21:25:27 +0900

    Add a configuration file for wezterm, terminal emulator

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,11 @@ return {
 	window_decorations		= "NONE",
 	enable_tab_bar			= false,
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 	exit_behavior			= "Close",
 >>>>>>> f8e852c (Add a configuration file for wezterm, terminal emulator)
+=======
+	exit_behavior			= "Close",
+>>>>>>> 51dd75d (Add a configuration file for wezterm, terminal emulator)
 }
