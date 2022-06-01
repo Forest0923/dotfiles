c15aaeeab279aaf27bc4abaa706d29e9213fbb38
Forest0923 <tropicalrainforest23@gmail.com>
2022-06-01 09:50:29 +0900

    Update preference of wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,15 @@
 return {
-	color_scheme			= "Tomorrow Night Bright",
+	--color_scheme			= "Tomorrow Night Bright",
+	colors = {
+		background	= "#000000",
+		foreground	= "#eaeaea",
+		ansi		= {'#000000', '#d54e53', '#23d18b', '#e6c547', '#3b8eea', '#c397d8', '#70c0ba', '#e5e5e5'},
+		bright		= {'#666666', '#ff3334', '#0dbc79', '#e7c547', '#2472c8', '#b77ee0', '#29b8db', '#e5e5e5'},
+		--cursor_bg	= "silver",
+		--cursor_fg	= "black",
+		--cursor_boarder	= "white",
+	},
+	--force_reverse_video_cursor	= true,
 	window_background_opacity	= 0.65,
 	text_background_opacity		= 1.0,
 	window_decorations		= "NONE",
