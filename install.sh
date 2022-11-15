550f73208f7c6e58242e4f8abc4cad686aa3e6ef
Forest0923 <tropicalrainforest23@gmail.com>
2022-11-15 10:12:30 +0900

    Fix: symbolic link command

--- install.sh
+++ install.sh
@@ -3,25 +3,26 @@ set -e
 
 source lib/inquirer.sh
 
+DOTFILES=`pwd`
+
 link_user() {
 	software=('bash' 'zsh' 'vim' 'neovim' 'tmux' 'alacritty' 'wezterm')
 	checkbox_input "Choose what you want!:" software selected_software
-	echo "`join selected_software`"
 
 	for i in ${selected_software[@]}; do
 		case $i in
-			"bash")	ln -snf .bashrc $HOME/.bashrc ;;
-			"zsh")	ln -snf .zshrc $HOME/.zshrc ;;
-			"vim")	ln -snf .vimrc $HOME/.vimrc ;;
+			"bash")	ln -snf $DOTFILES/.bashrc $HOME/.bashrc ;;
+			"zsh")	ln -snf $DOTFILES/.zshrc $HOME/.zshrc ;;
+			"vim")	ln -snf $DOTFILES/.vimrc $HOME/.vimrc ;;
 			"neovim")
 				if [ ! -d $HOME/.config/nvim ]; then
 					mkdir $HOME/.config/nvim
 				fi
-				ln -snf .vimrc $HOME/.config/nvim/init.vim
+				ln -snf $DOTFILES/.vimrc $HOME/.config/nvim/init.vim
 				;;
-			"tmux")	ln -snf .tmux.conf $HOME/.tmux.conf ;;
-			"alacritty")	ln -snf .alacritty.yml $HOME/.alacritty.yml ;;
-			"wezterm")	ln -snf .wezterm.lua $HOME/.wezterm.lua ;;
+			"tmux")	ln -snf $DOTFILES/.tmux.conf $HOME/.tmux.conf ;;
+			"alacritty")	ln -snf $DOTFILES/.alacritty.yml $HOME/.alacritty.yml ;;
+			"wezterm")	ln -snf $DOTFILES/.wezterm.lua $HOME/.wezterm.lua ;;
 			*) ;;
 		esac
 	done
@@ -30,20 +31,19 @@ link_user() {
 link_root() {
 	software=('bash' 'zsh' 'vim' 'neovim' 'tmux')
 	checkbox_input "Choose what you want!:" software selected_software
-	echo "`join selected_software`"
 
 	for i in ${selected_software[@]}; do
 		case $i in
-			"bash")	sudo ln -snf .bashrc /root/.bashrc ;;
-			"zsh")	sudo ln -snf .zshrc /root/.zshrc ;;
-			"vim")	sudo ln -snf .vimrc /root/.vimrc ;;
+			"bash")	sudo ln -snf $DOTFILES/.bashrc /root/.bashrc ;;
+			"zsh")	sudo ln -snf $DOTFILES/.zshrc /root/.zshrc ;;
+			"vim")	sudo ln -snf $DOTFILES/.vimrc /root/.vimrc ;;
 			"neovim")
 				if [ ! -d /root/.config/nvim ]; then
 					sudo mkdir /root/.config/nvim
 				fi
-				sudo ln -snf .vimrc /root/.config/nvim/init.vim
+				sudo ln -snf $DOTFILES/.vimrc /root/.config/nvim/init.vim
 				;;
-			"tmux")	sudo ln -snf .tmux.conf /root/.tmux.conf ;;
+			"tmux")	sudo ln -snf $DOTFILES/.tmux.conf /root/.tmux.conf ;;
 			*) ;;
 		esac
 	done
@@ -51,4 +51,6 @@ link_root() {
 
 target=('user' 'root')
 list_input "Choose your target environment:" target selected_target
+
+link_$selected_target
 
