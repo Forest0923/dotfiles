c688a6f3bb68ab747fd7600ee9918f8872c35a38
Forest0923 <tropicalrainforest23@gmail.com>
2023-04-01 19:24:51 +0900

    fix: bright -> brights

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,7 +3,7 @@ return {
 		background	= "#000000",
 		foreground	= "#eaeaea",
 		ansi		= {'#000000', '#d54e53', '#23d18b', '#e6c547', '#3b8eea', '#c397d8', '#70c0ba', '#e5e5e5'},
-		bright		= {'#666666', '#ff3334', '#0dbc79', '#e7c547', '#2472c8', '#b77ee0', '#29b8db', '#e5e5e5'},
+		brights		= {'#666666', '#ff3334', '#0dbc79', '#e7c547', '#2472c8', '#b77ee0', '#29b8db', '#e5e5e5'},
 	},
 	window_background_opacity	= 0.75,
 	text_background_opacity		= 1.0,
