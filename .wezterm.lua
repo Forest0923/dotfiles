fc2f5970d7e9492164e3a2862e11504940ea84cf
Forest0923 <masahirom0923@gmail.com>
2023-08-19 14:05:08 +0900

    wezterm: Update appearance

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,14 +1,28 @@
+local wezterm = require('wezterm')
+
 return {
+	font		= wezterm.font('Fira Code'),
+	font_size	= 15,
+
 	colors = {
 		background	= "#000000",
 		foreground	= "#eaeaea",
 		ansi		= {'#000000', '#d54e53', '#23d18b', '#e6c547', '#3b8eea', '#c397d8', '#70c0ba', '#e5e5e5'},
 		brights		= {'#666666', '#ff3334', '#0dbc79', '#e7c547', '#2472c8', '#b77ee0', '#29b8db', '#e5e5e5'},
 	},
+
+	window_padding = {
+		left	= 0,
+		right	= 0,
+		top	= 0,
+		bottom	= 0,
+	},
 	window_background_opacity	= 0.75,
 	text_background_opacity		= 1.0,
-	window_decorations		= "NONE",
-	enable_tab_bar			= false,
+	window_decorations		= "RESIZE",
+
+	enable_tab_bar			= true,
+	hide_tab_bar_if_only_one_tab	= true,
+
 	exit_behavior			= "Close",
-	font_size			= 14,
 }
